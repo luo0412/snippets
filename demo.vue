6bb72d8805abcfeecfa9f7823d05c7c9d6554211
luo0412 <1095847440@qq.com>
2026-03-06 00:26:40 +0800

    adjust

--- demo.vue
+++ demo.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="page">
-    <a-button type="primary" @click="visible = true">按钮</a-button>
-    <el-button type="primary" @click="visible = true">按钮</el-button>
-    <el-date-picker placeholder="请选择日期" v-model="date" />
+    <a-space>
+      <a-button type="primary" @click="visible = true">自定义按钮</a-button>
+      <el-button type="primary" @click="visible = true">按钮</el-button>
+      <el-date-picker placeholder="请选择日期" v-model="date" />
+    </a-space>
     <a-modal v-model:visible="visible" title="Hello world">
       <p>Try Ant Design Vue</p>
     </a-modal>
