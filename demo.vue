ecce4660f28e52b8f947ea7ea43880b61357bb2d
luo0412 <1095847440@qq.com>
2026-03-05 23:48:00 +0800

    update

---
+++ demo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page">
+    <a-button type="primary" @click="visible = true">按钮</a-button>
+    <el-button type="primary" @click="visible = true">按钮</el-button>
+    <el-date-picker placeholder="请选择日期" v-model="date" />
+    <a-modal v-model:visible="visible" title="Hello world">
+      <p>Try Ant Design Vue</p>
+    </a-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      date: null,
+      visible: false
+    }
+  }
+}
+</script>
+
+<style>
+.page {
+  padding: 20px;
+}
+</style>
